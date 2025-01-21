f3f91a12e83e04c5d2a355ef0d82964c29e26538
Yvan Kerdanet <yvan.kerdanet@gmail.com>
2025-01-21 15:12:39 +0100

    add registration page with form handling and API integration

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 // Import des différentes pages :
 import Home from './pages/Home';
 import Connexion from './pages/Connexion';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route index element={<Home />} /> {/* Définition de la page d'accueil sur la page Home */}
       <Route path='/connexion' element={<Connexion />} /> {/* Définition de la page de connexion sur la page Connexion */}
+      <Route path="/register" element={<Register />} /> {/* Définition de la page d'inscription sur la page Register */}
     </Routes>
   );
 }
--- front/src/pages/Register.jsx
+++ front/src/pages/Register.jsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Register() {
-  return (
-    <div>Register</div>
-  )
+    const [user, setUser] = useState({});
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setUser(user => ({...user, [name]: value}));
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('http://localhost:8000/api/user/signup', user);
+        } catch(e) {
+            console.log(e.message);
+        }
+    };
+
+    return (<>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='firstname'>Firstname</label>
+            <input
+                type='text'
+                name='firstname'
+                id='firstname'
+                onChange={handleChange}
+                placeholder='Firstname'
+            />
+            <label htmlFor='nom'>Lastname</label>
+            <input
+                type='text'
+                name='lastname'
+                id='lastname'
+                onChange={handleChange}
+                placeholder='Lastname'
+            />
+            <label htmlFor='avatar'>Avatar</label>
+            <input
+                type='text'
+                name='avatar'
+                id='avatar'
+                onChange={handleChange}
+                placeholder='Avatar URL'
+            />
+            <label htmlFor='email'>Email</label>
+            <input
+                type='email'
+                name='email'
+                id='email'
+                onChange={handleChange}
+                placeholder='Votre email'
+            />
+            <label htmlFor='password'>Password</label>
+            <input
+                type='password'
+                name='password'
+                id='password'
+                onChange={handleChange}
+                placeholder='Votre mot de passe'
+            />
+        </form>
+        <Link to='/connexion'>Already registered ?</Link>
+    </>)
 }
