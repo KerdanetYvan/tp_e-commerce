321c23d7e22bb1386df28928e15fc92741231468
Yvan Kerdanet <yvan.kerdanet@gmail.com>
2025-01-21 12:32:46 +0100

    add Redux integration and initial authentication context with basic pages

---
+++ front/src/context/AuthContext.jsx
@@ -0,0 +1,5 @@
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// Pour communiquer avec notre API
+import axios from "axios";
---
+++ front/src/pages/Detail.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Detail(id) {
+  return (
+    <div>Detail</div>
+  )
+}
